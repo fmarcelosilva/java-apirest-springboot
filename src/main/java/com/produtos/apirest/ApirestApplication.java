41880289b044ecca2e4f95b4d7563d5518f95153
Francisco Marcelo <fmarcelosilva@gmail.com>
2019-12-22 18:06:09 -0300

    Migrando versão inicial do Projeto para o Git

---
+++ src/main/java/com/produtos/apirest/ApirestApplication.java
@@ -0,0 +1,18 @@
+package com.produtos.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+		
+		System.out.println("===============================================");
+		System.out.println("Aplicação Inicializada com Sucesso             ");
+		System.out.println("===============================================");
+		
+	}
+
+}
